9f477b5421923d8d11cd6478d1ffdf572b6e7850
Mikhail Boichuk <mikhail.boichuk@gmail.com>
2024-10-15 14:43:46 +0300

    pw intro and overview #4

---
+++ pw-practice-app/tests/firstTest.spec.ts
@@ -0,0 +1,42 @@
+import {test} from '@playwright/test'
+import { url } from 'inspector'
+
+// Hook before all tests (more rarely used)
+test.beforeAll(() => {
+
+})
+
+// Hook before each test
+test.beforeEach(async ({page}) => {
+    await page.goto('http://localhost:4200/') // common action before each test including suites
+})
+
+test('navigate to layouts', async ({page}) => {
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('navigate to datepicker page', async ({page}) => {
+    await page.getByText('Forms').click()
+    await page.getByText('Datepicker').click()
+})
+
+test.afterEach(() => {
+
+})
+
+// Test suite structure
+test.describe('test suite one', () => {
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Charts').first().click() // common action before each test in this parcticular suite, executed after global beforeEach()
+    })
+    
+    test('navigate to echarts #1', async ({page}) => {
+        await page.getByText('Echarts').click()
+    })
+    
+    test('navigate to echarts #2', async ({page}) => {
+        await page.getByText('Echarts').click()
+    })
+})
+
