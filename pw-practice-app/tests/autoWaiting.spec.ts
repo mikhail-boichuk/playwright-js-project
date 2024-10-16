f6fcd7ad04f22447c22137f7b7ca9d9385ae26b3
Mikhail Boichuk <mikhail.boichuk@gmail.com>
2024-10-16 15:58:23 +0300

    basic interaction with web elements and waits/timeouts

---
+++ pw-practice-app/tests/autoWaiting.spec.ts
@@ -0,0 +1,61 @@
+import {expect, test} from '@playwright/test'
+
+test.beforeEach(async({page}, testInfo)=> {
+    await page.goto('http://uitestingplayground.com/ajax')
+    await page.getByText('Button Triggering AJAX Request').click()
+
+    testInfo.setTimeout(testInfo.timeout + 2000) // increase default test timemout by 2 seconds for each test in this spec file
+
+})
+
+test('Auto waiting', async({page}) => {
+    const successButton = page.locator('.bg-success')
+    
+    // await successButton.click()
+
+    // const text = await successButton.textContent() // this will wait for 30 seconds by default
+    // await successButton.waitFor({state: "attached"})
+    // const text = await successButton.allTextContents() // this will not wait by default
+
+    // expect(text).toContain('Data loaded with AJAX get request.')
+
+    // this will wait for 5 seconds by default, but we override to wait 20 seconds
+    await expect(successButton).toHaveText('Data loaded with AJAX get request.', {timeout: 20000})
+})
+
+test('Alternative waits', async({page}) => {
+    const successButton = page.locator('.bg-success')
+
+    // ___ wait for element
+    // await page.waitForSelector('.bg-success')
+
+    // ___ wait for particular response
+    // await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+
+    // ___ wait for network calls to be completed (NOT recommended)
+    await page.waitForLoadState('networkidle')
+
+    // hardcoded wait (NOT recommended)
+    // await page.waitForTimeout(25000)
+
+    const text = await successButton.allTextContents()
+    expect(text).toContain('Data loaded with AJAX get request.')
+
+})
+
+test('Timeouts', async({page}) => {
+    // ====Timeout types==== (can be set through playwright.config.ts)
+    // Global timeout - not set by default
+    // Test timeout - 30 seconds by default and no longer than Global timeout
+    // Action timeout (e.g. click(), fill()) - not set by default, but no longer than Test timeout
+    // Navigation timeout (e.g. page.goto('/')) - not set by default, but no longer than Test timeout
+    // Expect (assertion) timeout - 5 seconds by defualt and no longer than Test timeout
+
+    // test.setTimeout(10000) // override test timeout
+
+    test.slow() // increases default timeout 3 times
+
+    const successButton = page.locator('.bg-success')
+    await successButton.click({timeout: 16000}) //override action timeout
+
+})
--- pw-practice-app/tests/secondTest.spec.ts
+++ pw-practice-app/tests/secondTest.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test'
+import {expect, test} from '@playwright/test'
 
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
@@ -33,4 +33,109 @@ test('Locator syntax rules', async ({page}) => {
 
     //by exact text match
     page.locator(':text-is("Using the Grid")')
+})
+
+test('User facing locators', async ({page}) => {
+
+    await page.getByRole('textbox', {name: "Email"}).first().click()
+    await page.getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.getByLabel('Email').first().click()
+
+    await page.getByPlaceholder('Jane Doe').click()
+
+    await page.getByText('Using the Grid').click()
+
+    await page.getByTestId('SignIn').click()
+
+    await page.getByTitle('IoT Dashboard').click()
+
+})
+
+test('Locating child elements', async ({page}) => {
+
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+    await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click() // least preferable
+
+})
+
+test('Locating parent elements', async ({page}) => {
+
+    await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"}).click()
+    await page.locator('nb-card', {has: page.locator('#inputEmail1')}).getByRole('textbox', {name: "Email"}).click()
+
+    await page.locator('nb-card').filter({hasText: "Basic form"}).getByRole('textbox', {name: "Email"}).click()
+    await page.locator('nb-card').filter({has: page.locator('.status-danger')}).getByRole('textbox', {name: "Password"}).click()
+
+    await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: "Sign in"})
+        .getByRole('textbox', {name: "Email"}).click()
+
+    // not recommended as it uses XPath to step one level up in the DOM
+    await page.locator(':test-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Email"}).click()
+
+})
+
+test('Reusing the locators', async ({page}) => {
+
+    const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+    const emailField = basicForm.getByRole('textbox', {name: "Email"})
+
+    await emailField.fill('test@test.com')
+    await basicForm.getByRole('textbox', {name: "Password"}).fill('Welcome123')
+    await basicForm.locator('nb-checkbox').click()
+    await basicForm.filter({hasText: "Basic form"}).getByRole('button').click()
+
+    await expect(emailField).toHaveValue('test@test.com')
+
+})
+
+test('Extracting values', async ({page}) => {
+
+    //single text value
+    const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+    const buttonText = await basicForm.locator('button').textContent()
+
+    expect(buttonText).toEqual('Submit2')
+
+    //all text values
+    const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents()
+
+    expect(allRadioButtonsLabels).toContain('Option 1')
+
+    //input field value
+    const emailField = basicForm.getByRole('textbox', {name: "Email"})
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+
+    expect(emailValue).toEqual('test@test.com')
+
+    //attribute value
+    const placeholderValue = await emailField.getAttribute('placeholder')
+
+    expect(placeholderValue).toEqual('Email')
+
+})
+
+test('Assertions', async ({page}) => {
+
+    //General assertions
+    const value = 5
+    expect(value).toEqual(5)
+
+    const basicFormButton = page.locator('nb-card').filter({hasText: "Basic form"}).locator('button')
+    const text = await basicFormButton.textContent()
+
+    expect(text).toEqual('Submit')
+
+    //Locator assertion
+    await expect(basicFormButton).toHaveText('Submit') //will wait for up to 5 seconds by default
+
+    //Soft assertion
+    await expect.soft(basicFormButton).toHaveText('Submit44') //will fail the test, but test will continue execution
+    await basicFormButton.click()
+
 })
