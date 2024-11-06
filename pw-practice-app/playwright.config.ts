aaaa6172e0a0e4253fdc8b4a957b1f342c7ff47e
Mikhail Boichuk <mikhail.boichuk@gmail.com>
2024-11-06 13:15:01 +0200

    reporting

--- pw-practice-app/playwright.config.ts
+++ pw-practice-app/playwright.config.ts
@@ -30,8 +30,15 @@ export default defineConfig<TestOptions>({
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['json', {outputFile: 'test-results/jsonReport.json'}],
+    ['junit', {outputFile: 'test-results/junitReport.xml'}],
+    ['html']
+  ],
+
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
